855e5b976cea597700fe049b7782c6665c1b7872
Arthur Bottemanne <walvieswisel@gmail.com>
2024-11-02 10:40:51 +0100

    feat: add transactions page

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         "body-gray": "#F0F0F0",
         "primary-gray": "#323236",
         "secondary-gray": "#83848B",
+        "icon-gray": "#EFF1F7",
       },
       boxShadow: {
         navbar: "0 10px 10px 4px rgb(0, 0, 0)",
